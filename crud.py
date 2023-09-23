c268a8dec4825ea2d4d36f76d5bcc0c871e8ba62
T6nisValk <131583739+T6nisValk@users.noreply.github.com>
2023-09-23 14:02:18 +0300

    Create tables

--- crud.py
+++ crud.py
@@ -1,1 +1,22 @@
-create_database_query = "CREATE DATABASE IF NOT EXISTS todo_app"
+create_database_query = """CREATE DATABASE IF NOT EXISTS todo_app"""
+
+create_tables_query = """CREATE TABLE IF NOT EXISTS todo_lists(
+    list_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS todo_items(
+    item_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    state BOOLEAN NOT NULL,
+    list_id INT NOT NULL,
+    FOREIGN KEY (list_id) REFERENCES todo_lists(list_id)
+);
+"""
+
+
+def add_todo_list_query(title):
+    return f"INSERT INTO todo_lists (title) VALUES ('{title}');"
+
+
+def create_item_to_list(list_id, name):
+    return f"INSERT INTO todo_items (name, state, list_id) VALUES ('{name}', False, '{list_id}')"
--- main.py
+++ main.py
@@ -1,10 +1,61 @@
 from mysql.connector import connect, Error
-from crud import create_database_query
+from crud import (
+    create_database_query,
+    create_tables_query,
+    add_todo_list_query,
+    create_item_to_list,
+)
 
-try:
-    with connect(host="localhost", user="admin", password="admin", port="3306") as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(create_database_query)
 
-except Error as e:
-    print(e)
+def setup_db():
+    try:
+        with connect(
+            host="localhost", user="admin", password="admin", port="3306"
+        ) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(create_database_query)
+                cursor.execute("USE todo_app")
+                cursor.execute(create_tables_query)
+
+    except Error as e:
+        print(e)
+
+
+def create_todo_list(title):
+    try:
+        with connect(
+            host="localhost",
+            user="admin",
+            password="admin",
+            port="3306",
+            database="todo_app",
+        ) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(add_todo_list_query(title))
+                conn.commit()
+    except Error as e:
+        print(e)
+
+
+def create_task(list_id, name):
+    try:
+        with connect(
+            host="localhost",
+            user="admin",
+            password="admin",
+            port="3306",
+            database="todo_app",
+        ) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(create_item_to_list(list_id, name))
+    except Error as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    setup_db()
+    # list_name = input("Enter the name of the list: ")
+    # create_todo_list(list_name)
+
+    task_title = input("Task title: ")
+    create_task(1, task_title)
