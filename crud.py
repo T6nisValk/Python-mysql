67c3f235d62ff848cd57374889a0c235530d3a56
T6nisValk <131583739+T6nisValk@users.noreply.github.com>
2023-09-24 09:46:42 +0300

    New stuff

--- crud.py
+++ crud.py
@@ -1,6 +1,8 @@
-create_database_query = """CREATE DATABASE IF NOT EXISTS todo_app"""
+create_database_query = """--sql
+CREATE DATABASE IF NOT EXISTS todo_app"""
 
-create_tables_query = """CREATE TABLE IF NOT EXISTS todo_lists(
+create_tables_query = """--sql
+CREATE TABLE IF NOT EXISTS todo_lists(
     list_id INT PRIMARY KEY AUTO_INCREMENT, 
     title VARCHAR(255) NOT NULL
 );
@@ -15,8 +17,14 @@ CREATE TABLE IF NOT EXISTS todo_items(
 
 
 def add_todo_list_query(title):
-    return f"INSERT INTO todo_lists (title) VALUES ('{title}');"
+    return f"""--sql
+INSERT INTO todo_lists (title) VALUES ('{title}');"""
 
 
 def create_item_to_list(list_id, name):
-    return f"INSERT INTO todo_items (name, state, list_id) VALUES ('{name}', False, '{list_id}')"
+    return f"""--sql
+INSERT INTO todo_items (name, state, list_id) VALUES ('{name}', False, '{list_id}')"""
+
+
+select_all_task_query = """--sql
+SELECT * FROM todo_items"""
--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from crud import (
     create_tables_query,
     add_todo_list_query,
     create_item_to_list,
+    select_all_task_query,
 )
 
 
 def setup_db():
     try:
-        with connect(
-            host="localhost", user="admin", password="admin", port="3306"
-        ) as conn:
+        with connect(host="localhost", user="admin", password="admin", port="3306") as conn:
             with conn.cursor() as cursor:
                 cursor.execute(create_database_query)
                 cursor.execute("USE todo_app")
@@ -48,6 +47,23 @@ def create_task(list_id, name):
         ) as conn:
             with conn.cursor() as cursor:
                 cursor.execute(create_item_to_list(list_id, name))
+                conn.commit()
+    except Error as e:
+        print(e)
+
+
+def select_task(list_id, name):
+    try:
+        with connect(
+            host="localhost",
+            user="admin",
+            password="admin",
+            port="3306",
+            database="todo_app",
+        ) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(select_all_task_query(list_id, name))
+                print(cursor.fetchall())
     except Error as e:
         print(e)
 
@@ -57,5 +73,6 @@ if __name__ == "__main__":
     # list_name = input("Enter the name of the list: ")
     # create_todo_list(list_name)
 
-    task_title = input("Task title: ")
-    create_task(1, task_title)
+    # task_title = input("Task title: ")
+    # create_task(1, task_title)
+    select_task()
